b069c844f69299c90ab5d532bbbc8e8c5d184e35
Alok Singh <alok.singh9711@gmail.com>
2024-05-19 10:49:29 +0530

    Removed some extra code from the app.js

--- app.js
+++ app.js
@@ -27,16 +27,6 @@ const storage = multer.diskStorage({
     }
 });
 
-const storageL = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, movieDir)
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
-
 const upload = multer({ storage: storage });
 
 const options = {
